eb8c9180fc071be79b7c4b43669a72d847e39f3a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-01-22 21:25:03 +0800

    kernel: throne_tracker: re-initialize apk_path_hash_list on search_manager
    re-initialize apk_path_hash_list in search_manager to avoid stale data
    
    seems shit happens when no manager is installed and a user installs one later
    is the data stale? is it because I include five hashes? <we_just_dont_know.gif>
    
    cleanup of https://github.com/xiaomi-sdm678/android_kernel_xiaomi_mojito/commit/754bba236c599b64eb04965d4ec8e7f110c53208
    
    Change-Id: I77ccf6fc80af7bb9b4ea716462466075b67c5c76
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: fadlyas07 <mhmmdfdlyas@proton.me>

--- kernel/throne_tracker.c
+++ kernel/throne_tracker.c
@@ -213,8 +213,9 @@ void search_manager(const char *path, int depth, struct list_head *uid_data)
 	int i, stop = 0;
 	struct list_head data_path_list;
 	INIT_LIST_HEAD(&data_path_list);
+	INIT_LIST_HEAD(&apk_path_hash_list);
 	unsigned long data_app_magic = 0;
-	
+
 	// Initialize APK cache list
 	struct apk_path_hash *pos, *n;
 	list_for_each_entry(pos, &apk_path_hash_list, list) {
